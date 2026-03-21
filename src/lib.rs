bb633e1a959def1a613f4ef81778008c7617ce6d
Simon Deurell <simon@deurell.se>
2026-03-21 09:45:30 +0100

    Use click_and_drag sense for hover blocker to prevent text selection

--- src/lib.rs
+++ src/lib.rs
@@ -288,7 +288,7 @@ impl PieMenu {
             .order(egui::Order::Foreground)
             .fixed_pos(egui::Pos2::ZERO)
             .show(ctx, |ui| {
-                ui.allocate_rect(screen_rect, egui::Sense::hover());
+                ui.allocate_rect(screen_rect, egui::Sense::click_and_drag());
             });
 
         let painter = ctx.layer_painter(egui::LayerId::new(egui::Order::Tooltip, self.id));
